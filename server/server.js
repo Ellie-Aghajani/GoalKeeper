5c643aedc555b4ae1b6f4e8625021130fcaa6bec
Ellie-Aghajani <z.aqajani@gmail.com>
2023-01-31 15:57:14 -0800

    added new list object

--- server/server.js
+++ server/server.js
@@ -1,53 +1,81 @@
 const express = require('express');
 const morgan = require('morgan');
+// const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-const habit = [
-    {
-        id:"abc",
-        task:'reading',
-        completed:false
-    },
-    {
-        id:"def",
-        task:'walking',
-        completed:true
-    },
+// in-memory habits
+const habits = [
+  {
+    id: 'abc',
+    name: 'reading',
+    description: 'reading 30 pages a day',
+    frequency: "Sunday",
+    completed : false
+  },
+  {
+    id: 'def',
+    name: 'exercise',
+    description: 'exercising 30 minutes a day',
+    frequency: "Sunday",
+    completed : false
+  },
 ];
-//middleware
+
+// middleware
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
- 
-//get
-app.get('/todos', (req, res) =>{
-    res.json(habit);
-})
+app.use(express.urlencoded({ extended: false }));
+// app.use(cors());
+app.use(express.static('../client/build'));
 
-//POST 
+// routes
 
-app.post('/todos',(req,res)=>{
-    console.log(req.body);
-    const task = req.body.task;
-    const id = Math.random().toString(36).substring(2,5);
-    const completed = false;
-    const newTodo = {
-        task,
-        completed,
-        id:id,
-    };
-    todos.push(newTodo);
-    res.json(newTodo);
-
+// GET /habits
+app.get('/habits', (req, res) => {
+  res.json(habits);
 });
 
-//PATCH/todos/:id
-app.patch('/todos/:id',(req,res)=> {
-    const id = req.params.id;
-})
+// POST /habits
+app.post('/habits', (req, res) => {
+ 
+  const id = Math.random().toString(36).substring(2, 5);
+  const name = req.body.name;
+  const description= req.body.description;
+  const frequency = req.body.frequency;
+  const completed = false;
 
-app.listen(port,() => {
-    console.log(`app is listening on port ${port}`)
+
+  const newHabit = {
+    
+    id: id,
+    name: name,
+    description: description,
+    frequency: frequency,
+    completed : completed
+  };
+
+  habits.push(newHabit);
+
+  res.json(newHabit);
+});
+
+// PATCH /habits/:id
+app.patch('/habits/:id', (req, res) => {
+  const id = req.params.id;
+
+  // find the appropriate todo
+  const foundHabit = habits.find((habit) => habit.id === id);
+
+  console.log(foundHabit);
+
+  // foundTodo.completed = true;
+  foundHabit.completed = !foundHabit.completed;
+
+  res.status(204).send();
+});
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
 });
